87295b5cbc742305d43a1939f15eeb3dbe20274b
JustKom <halchenko.des@gmail.com>
2022-12-27 03:19:13 +0200

    hits block

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,4 @@
-$(function() {
+$(function () {
 
   $('.top-slider__inner').slick({
     dots: true,
@@ -6,6 +6,18 @@ $(function() {
     fade: true,
     autoplay: true,
     autoplaySpeed: 5000
+  });
+
+  $('.new-slider__inner').slick({
+    centerMode: true,
+    // centerPadding: '60px',
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    infinite: true,
   });
 
 });
